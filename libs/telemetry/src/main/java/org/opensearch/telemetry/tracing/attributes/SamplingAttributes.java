61a4f6a1cac9b21962fcbf4ce559cdd670e6e44f
Nishchay Malhotra <nishcha@amazon.com>
2024-03-29 11:29:09 +0530

    Fixing comments
    Signed-off-by: Nishchay Malhotra <nishcha@amazon.com>
    
    Adding new sampled attributes

---
+++ libs/telemetry/src/main/java/org/opensearch/telemetry/tracing/attributes/SamplingAttributes.java
@@ -0,0 +1,41 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.opensearch.telemetry.tracing.attributes;
+
+import org.opensearch.common.annotation.InternalApi;
+
+import java.util.Locale;
+
+/**
+ * Enum for Inferred Sampling*
+ * @opensearch.internal*
+ */
+@InternalApi
+public enum SamplingAttributes {
+
+    /**
+     * Attribute added if the span is sampled by inferred sampler*
+     */
+    INFERRED_SAMPLER,
+
+    /**
+     * Attribute Added if the span is an outlier*
+     */
+    SAMPLED,
+
+    /**
+     * Sampler Used in the framework*
+     */
+    SAMPLER;
+
+    @Override
+    public String toString() {
+        return name().toLowerCase(Locale.getDefault());
+    }
+}
