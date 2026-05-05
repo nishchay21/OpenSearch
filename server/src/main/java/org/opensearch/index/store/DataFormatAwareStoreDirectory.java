12ec5ddddae7bac99182abef0949371f36c10b4e
Kamal Nayan <askkamal@amazon.com>
2026-05-05 21:59:11 +0530

    Add DataFormatAwareMergeCheckpointPublisher and catalog generation bump on engine open
    Signed-off-by: Kamal Nayan <askkamal@amazon.com>

--- server/src/main/java/org/opensearch/index/store/DataFormatAwareStoreDirectory.java
+++ server/src/main/java/org/opensearch/index/store/DataFormatAwareStoreDirectory.java
@@ -221,7 +221,7 @@ public class DataFormatAwareStoreDirectory extends FilterDirectory {
      */
     private long calculateChecksum(FileMetadata fm) throws IOException {
         String fileIdentifier = toFileIdentifier(fm);
-        FormatChecksumStrategy strategy = checksumStrategies.getOrDefault(fm.dataFormat(), DEFAULT_CHECKSUM_STRATEGY);
+        FormatChecksumStrategy strategy = checksumStrategies.get(fm.dataFormat());
         return strategy.computeChecksum(this, fileIdentifier);
     }
 
